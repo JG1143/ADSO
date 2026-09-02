a4ba26790bd5dc05491b2d80dffecdc85f00ad8b
unknown <luisabreoangel123@gmail.com>
2026-09-02 10:54:39 -0500

    Se agregaro la importacion del modulo de rutas al archivo app

--- API/app.py
+++ API/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from Config import Config
+from Routes import loadRouters
+
 
 app = Flask(__name__)
-# blueprint
 app.config.from_object(Config) 
 mysql = MySQL(app)
 
+loadRouters(app)
 app.run(debug=True, port=5000, host="0.0.0.0")
